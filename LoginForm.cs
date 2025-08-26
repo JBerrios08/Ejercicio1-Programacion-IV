d9fb079b644d9206757e6564ab1e8bf429e3d179
Jaime Fernando Berrios Ortiz <162358684+JBerrios08@users.noreply.github.com>
2025-08-26 05:23:16 -0600

    Add basic LoginForm

---
+++ LoginForm.cs
@@ -0,0 +1,12 @@
+using System.Windows.Forms;
+
+namespace Ejercicio1
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
